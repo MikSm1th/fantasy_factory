530c191a6b31b75f047ac937a9ab61e0db5b7220
MikSm1th <thiscuriousquest@gmail.com>
2021-10-19 19:42:15 -0400

    Tue 19 Oct 2021 07:42:15 PM EDT

--- lineupopt/functions/clean_data.py
+++ lineupopt/functions/clean_data.py
@@ -8,5 +8,5 @@ from datetime import date
 today = date.today()
 
 def archive_files(): 
-    for file in glob.glob('./data/nfl/*.csv'):
-       shutil.move(file, './data/nfl/archive/%s/' % (today.strftime("%Y%m%d"))) 
+    for file in glob.glob('/home/michael/fan_fact/lineupopt/data/nfl/*.csv'):
+       shutil.move(file, '/home/michael/fan_fact/lineupopt/data/nfl/archive/%s/%s' % (today.strftime("%Y%m%d"), file)) 
